a6c1894bc73ce138153b94eb2baebac914469792
jonesyjava <jonesyjava@gmail.com>
2021-02-18 17:08:22 -0700

    price added

--- app/Models/Item.js
+++ app/Models/Item.js
@@ -9,7 +9,7 @@ export default class Item{
     get Template (){
         return /*html*/ `<div class="col-3 card-body">
         <h5 class="card-title">${this.name}</h5>
-        <p class="card-text">Price: $100</p>
+        <p class="card-text">${this.price}</p>
         <p class="card-text">${this.quantity} <span id="VANSquant"></span></p>
         <button class="btn btn-success" onclick="app.shopController.purchase('${this.name}')">Buy VANS</button>
     </div>`
